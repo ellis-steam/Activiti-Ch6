3fd57e245758b2b729deadb2b9c6b10828617e83
Ellis <ellis@s-team.com.tw>
2016-07-12 10:04:19 +0800

    6.1.3. Implementing standalone subprocess 6.1.2. Implementing embedded subprocess also included.

---
+++ src/main/java/org/bpmnwithactiviti/chapter06/callactivity/CreditCheckService.java
@@ -0,0 +1,18 @@
+package org.bpmnwithactiviti.chapter06.callactivity;
+
+import org.activiti.engine.delegate.DelegateExecution;
+import org.activiti.engine.delegate.JavaDelegate;
+
+public class CreditCheckService implements JavaDelegate {
+
+	@Override
+	public void execute(DelegateExecution execution) throws Exception {
+		boolean approved = false;
+		String customerID = (String) execution.getVariable("customerID");
+		int intCustomer = Integer.parseInt(customerID);
+		if(intCustomer > 1000 && intCustomer < 10000) {
+			approved = true;
+		}
+		execution.setVariable("creditCheckApproved", approved);
+	}
+}
